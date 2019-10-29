c4f1288dda022906d79c51133d8b0a97eb4e4881
Your Name <nur.mnzaman@gmail.com>
2019-10-30 04:12:48 +0600

    check

---
+++ app/src/main/java/com/zaman/androidsqlite/ADD_PRODUCT_LAYOUT.java
@@ -0,0 +1,13 @@
+package com.zaman.androidsqlite;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ADD_PRODUCT_LAYOUT extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add__product__layout);
+    }
+}
---
+++ app/src/main/java/com/zaman/androidsqlite/PRODUCT_LIST_LAYOUT.java
@@ -0,0 +1,13 @@
+package com.zaman.androidsqlite;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PRODUCT_LIST_LAYOUT extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product__list__layout);
+    }
+}
